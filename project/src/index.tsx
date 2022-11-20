f96792f5bf77e3e896bae642bf682a9d59a34d29
Mykhailo Movchan <mihmov@gmail.com>
2022-11-20 21:14:31 +0000

    Создал и подключил первый компонент для главного экрана

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-// const i = 3;
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
