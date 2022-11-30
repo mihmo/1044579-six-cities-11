e1a8dfe77e0f9036964a14f6dc2b473722819724
Mykhailo Movchan <mihmov@gmail.com>
2022-11-30 16:28:28 +0000

    включил маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,42 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../consts';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   placeCardCount: number;
 }
 
 function App({placeCardCount}: AppProps): JSX.Element {
-  return <Main placeCardCount={placeCardCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main placeCardCount={placeCardCount} ></Main>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<Property />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/consts.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Login = '/login',
+  Main = '/',
+  Favorites = '/favorites',
+  Property = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
