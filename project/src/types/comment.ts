a6aa0a7038bf89b8f51180752a1c6e8750a89ce3
Mykhailo Movchan <mihmov@gmail.com>
2022-12-03 23:24:27 +0000

    Добавляет новый компонент add-comment

---
+++ project/src/types/comment.ts
@@ -0,0 +1,17 @@
+export type Comment = {
+  comment: string;
+  date: string;
+  id: number;
+  rating: number;
+  user: {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+  };
+};
+
+export type NewComment = {
+  comment: string;
+  rating: number;
+};
