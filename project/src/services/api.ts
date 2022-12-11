1f414295567bd464b35d0bfd73aff7918591699e
Mykhailo Movchan <mihmov@gmail.com>
2022-12-11 18:01:28 +0000

    7.1 Сконфигурирует пакет axios.

---
+++ project/src/services/api.ts
@@ -0,0 +1,14 @@
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import {getToken} from './token';
+
+const BACKEND_URL = 'https://11.react.pages.academy/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  return api;
+};
